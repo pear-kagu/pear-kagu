7d6c0c47bd874fa94041c9f18ee68ff36cc0fbe7
Tatiana Scott <hello@tatianascott.com>
2019-01-28 12:56:56 -0500

    chore: merge master

---
+++ server/db/models/location.js
@@ -0,0 +1,30 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const Location = db.define('location', {
+  city: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      isAlpha: true
+    }
+  },
+  state: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      isAlpha: true
+    }
+  },
+  zipcode: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    validate: {
+      isInt: true,
+      max: 5,
+      min: 5
+    }
+  }
+})
+
+module.exports = Location
