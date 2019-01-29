ef0998e352427fd3ab92f9a6476b640d9e002b7c
Emily Quinn <emkquinn@gmail.com>
2019-01-29 11:35:48 -0500

    Feat: added foundations for Landing component and fixed naming convention of other components to use capitalization

---
+++ client/components/Landing.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react'
+import PropTypes from 'prop-types'
+import {connect} from 'react-redux'
+// import {Link} from 'react-router-dom'
+
+class Landing extends Component {
+  constructor() {
+    super()
+  }
+
+  render() {
+    return (
+      <div>
+        <div>Javascript</div>
+        <div>Learn how to code</div>
+        <dif>Women in code</dif>
+      </div>
+    )
+  }
+}
+
+/**
+ * CONTAINER
+ */
+
+export default Landing
+
+/**
+ * PROP TYPES
+ */
+Landing.propTypes = {
+  //   handleClick: PropTypes.func.isRequired,
+  //   isLoggedIn: PropTypes.bool.isRequired
+}
