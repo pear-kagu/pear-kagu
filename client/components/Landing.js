d63156f7618b446405f4d9d437cfb03c94ec875b
Emily Quinn <emkquinn@gmail.com>
2019-01-29 11:41:59 -0500

    Feat: Landing page component rendering hard-coded messages

--- client/components/Landing.js
+++ client/components/Landing.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react'
+import {Link} from 'react-router-dom'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
 // import {Link} from 'react-router-dom'
@@ -11,6 +12,7 @@ class Landing extends Component {
   render() {
     return (
       <div>
+        <h2>Welcome! Select an interest to get started</h2>
         <div>Javascript</div>
         <div>Learn how to code</div>
         <dif>Women in code</dif>
