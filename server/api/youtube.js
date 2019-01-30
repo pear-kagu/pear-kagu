d23c01dc723e171a9a4f90374fc50055d9033345
VandyWebDev <39500384+VandyWebDev@users.noreply.github.com>
2019-01-30 12:19:31 -0500

    chore: save data fetched from youtube api to Content model

--- server/api/youtube.js
+++ server/api/youtube.js
@@ -1,7 +1,12 @@
 const router = require('express').Router()
 const {google} = require('googleapis')
 const youtubeApiKey = process.env.YOUTUBEAPI_KEY
+const {Content} = require('../db/models')
 module.exports = router
+
+// set paramaters
+const API_SOURCE_ID = 1
+const TYPE_ID = 2
 
 var youtube = google.youtube({
   version: 'v3',
@@ -15,10 +20,33 @@ router.get('/:topic', async (req, res, next) => {
       part: 'id,snippet',
       q: topic,
       maxResults: 50,
-      regionCode: 'US'
+      regionCode: 'US',
+      relevanceLanguage: 'en'
     })
-    console.log(searchResult.data)
-    res.json(searchResult.data)
+
+    // deconstruct data
+    const {items} = searchResult.data
+    await Promise.all(
+      items.map(item => {
+        const title = item.snippet.title
+        const sourceUrl = `https://www.youtube.com/watch?v=${item.id.videoId}`
+        const imageUrl =
+          item.snippet.thumbnails.medium.url ||
+          'https://images.pexels.com/photos/97077/pexels-photo-97077.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500'
+        const description = item.snippet.description
+        const publishedAt = item.snippet.publishedAt
+        Content.create({
+          title,
+          imageUrl,
+          description,
+          sourceUrl,
+          publishedAt,
+          typeId: TYPE_ID,
+          apiSourceId: API_SOURCE_ID
+        })
+      })
+    )
+    res.json(items)
   } catch (err) {
     console.log(err)
   }
