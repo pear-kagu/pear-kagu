9d6f6aedc504cef284512208ae7baac91e44be4f
Tatiana Scott <hello@tatianascott.com>
2019-01-30 11:44:55 -0500

    imported Component

--- client/components/SavedContentPage.js
+++ client/components/SavedContentPage.js
@@ -1,8 +1,8 @@
-import React from 'react'
+import React, {Component} from 'react'
 
 const interests = ['Javascript', 'Python', 'CSS', 'HTML']
 
-class SavedContentPage extends React.Component {
+class SavedContentPage extends Component {
   render() {
     return (
       <div>
