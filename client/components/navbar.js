0b1dbb5b54eb4dae9e7cbaae91ad48f6dcf6e50b
Tatiana Scott <hello@tatianascott.com>
2019-01-30 13:10:20 -0500

    nav-bar styling

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -5,43 +5,150 @@ import {Link} from 'react-router-dom'
 import {logout} from '../store'
 import {Login} from '../components'
 
-class Navbar extends Component {
-  constructor(props) {
-    super(props)
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import IconButton from '@material-ui/core/IconButton'
+import Typography from '@material-ui/core/Typography'
+import InputBase from '@material-ui/core/InputBase'
+import Badge from '@material-ui/core/Badge'
+import MenuItem from '@material-ui/core/MenuItem'
+import Menu from '@material-ui/core/Menu'
+import {fade} from '@material-ui/core/styles/colorManipulator'
+import {withStyles} from '@material-ui/core/styles'
+import MenuIcon from '@material-ui/icons/Menu'
+import SearchIcon from '@material-ui/icons/Search'
+import AccountCircle from '@material-ui/icons/AccountCircle'
+import MailIcon from '@material-ui/icons/Mail'
+import NotificationsIcon from '@material-ui/icons/Notifications'
+import MoreIcon from '@material-ui/icons/MoreVert'
+import TextField from '@material-ui/core/TextField'
+
+const styles = theme => ({
+  root: {
+    width: '100%'
+  },
+  grow: {
+    flexGrow: 1
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20
+  },
+  textField: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit
+  },
+  title: {
+    display: 'none',
+    [theme.breakpoints.up('sm')]: {
+      display: 'block'
+    }
+  },
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25)
+    },
+    marginRight: theme.spacing.unit * 2,
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing.unit * 3,
+      width: 'auto'
+    }
+  },
+  searchIcon: {
+    width: theme.spacing.unit * 9,
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  inputRoot: {
+    color: 'inherit',
+    width: '100%'
+  },
+  inputInput: {
+    paddingTop: theme.spacing.unit,
+    paddingRight: theme.spacing.unit,
+    paddingBottom: theme.spacing.unit,
+    paddingLeft: theme.spacing.unit * 10,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('md')]: {
+      width: 200
+    }
+  },
+  sectionDesktop: {
+    display: 'none',
+    [theme.breakpoints.up('md')]: {
+      display: 'flex'
+    }
+  },
+  sectionMobile: {
+    display: 'flex',
+    [theme.breakpoints.up('md')]: {
+      display: 'none'
+    }
   }
+})
+
+class Navbar extends Component {
+  // constructor(props) {
+  //   super(props)
+  // }
 
   render() {
+    console.log(this.props)
+    const {classes} = this.props
+
     return (
-      <div>
-        <nav className="navbar">
-          <Link to="/" id="homeIcon">
-            <h1>Pear-Kagu</h1>
-          </Link>
-          <div>
-            <input type="text" id="searc" name="search" />
-            <button type="submit">
-              <label htmlFor="search">Search</label>
-            </button>
-          </div>
-          {this.props.isLoggedIn ? (
-            <div>
-              {/* The navbar will show these links after you log in */}
-              <h4>My Favorites</h4>
-              <a href="#" onClick={this.props.handleClick}>
-                Logout
-              </a>
-              <Link to="/savedcontent">Saved Content</Link>
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography
+              className={classes.title}
+              variant="h6"
+              color="inherit"
+              noWrap
+            >
+              Pear Kagu
+            </Typography>
+            <div className={classes.search}>
+              <div className={classes.searchIcon}>
+                <SearchIcon />
+              </div>
+              <InputBase
+                placeholder="Search…"
+                classes={{
+                  root: classes.inputRoot,
+                  input: classes.inputInput
+                }}
+              />
             </div>
-          ) : (
-            <div>
-              {/* The navbar will show these links before you log in */}
-              <Login name="login" />
-              {/* <Link to="/login">Login</Link> */}
-              <Link to="/signup">Sign Up</Link>
-            </div>
-          )}
-        </nav>
-        <hr />
+            {this.props.isLoggedIn ? (
+              <div>
+                {/* The navbar will show these links after you log in */}
+                <h4>My Favorites</h4>
+                <a href="#" onClick={this.props.handleClick}>
+                  Logout
+                </a>
+                <Link to="/savedcontent">Saved Content</Link>
+              </div>
+            ) : (
+              <div>
+                {/* The navbar will show these links before you log in */}
+                <Login name="login" />
+                <Link to="/signup">Sign Up</Link>
+              </div>
+            )}
+            <div className={classes.grow} />
+          </Toolbar>
+        </AppBar>
       </div>
     )
   }
@@ -64,12 +171,13 @@ const mapDispatch = dispatch => {
   }
 }
 
-export default connect(mapState, mapDispatch)(Navbar)
+export default connect(mapState, mapDispatch)(withStyles(styles)(Navbar))
 
 /**
  * PROP TYPES
  */
 Navbar.propTypes = {
   handleClick: PropTypes.func.isRequired,
-  isLoggedIn: PropTypes.bool.isRequired
+  isLoggedIn: PropTypes.bool.isRequired,
+  classes: PropTypes.object.isRequired
 }
