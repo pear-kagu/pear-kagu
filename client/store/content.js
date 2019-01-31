62aae36946e4136e886a3dfc9b55545ad9c3de76
Tatiana Scott <hello@tatianascott.com>
2019-01-31 16:46:04 -0500

    content store and api route

--- client/store/content.js
+++ client/store/content.js
@@ -1,21 +1,68 @@
 import axios from 'axios'
 
 //action types
-const GET_CONTENT = 'GET_CONTENT'
+const GET_MEETUPS = 'GET_MEETUPS'
+const GET_YOUTUBES = 'GET_YOUTUBES'
+const GET_NEWS = 'GET_NEWS'
 
 //action creators
-const getContent = content => {
+const getMeetups = meetups => {
   return {
-    type: GET_CONTENT,
-    content
+    type: GET_MEETUPS,
+    meetups
+  }
+}
+
+const getYoutubes = youtubes => {
+  return {
+    type: GET_YOUTUBES,
+    youtubes
+  }
+}
+
+const getNews = news => {
+  return {
+    type: GET_NEWS,
+    news
   }
 }
 
 //thunk creators
-export const fetchMeetups = () => {
-  return async dispath => {}
+export const fetchContent = (typeId, interestName) => {
+  return async dispatch => {
+    const {data} = await axios.get(`/api/content/${typeId}/${interestName}`)
+    if (typeId === 1) {
+      dispatch(getNews(data))
+    } else if (typeId === 2) {
+      dispatch(getYoutubes(data))
+    } else if (typeId === 3) {
+      dispatch(getMeetups(data))
+    }
+  }
 }
 
 //initial state
+const initialState = {
+  read: [],
+  watch: [],
+  do: []
+}
 
 //reducer
+
+export default (state = initialState, action) => {
+  const newState = {...state}
+  switch (action.type) {
+    case GET_MEETUPS:
+      newState.do = action.meetups
+      return newState
+    case GET_YOUTUBES:
+      newState.watch = action.youtubes
+      return newState
+    case GET_NEWS:
+      newState.read = action.news
+      return newState
+    default:
+      return state
+  }
+}
