9eacac035d39a9e5ff83d3ace48896e23886718f
VandyWebDev <39500384+VandyWebDev@users.noreply.github.com>
2019-02-02 10:51:43 -0500

    feat: user api route to get saved content for the user

--- server/api/content.js
+++ server/api/content.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
-const {Content, Interest, User} = require('../db/models')
+const {Content} = require('../db/models')
 
 module.exports = router
 
@@ -16,29 +16,6 @@ router.get('/:typeId/:interestId', async (req, res, next) => {
     })
 
     res.status(200).send(content)
-  } catch (err) {
-    console.error(err)
-  }
-})
-
-router.get('/:userId', async (req, res, next) => {
-  try {
-    let userId = Number(req.params.userId)
-    let savedContent = await User.findAll({
-      where: {
-        id: userId
-      },
-      attributes: ['id'],
-      include: [
-        {
-          model: Content,
-          attributes: ['title', 'sourceUrl', 'imageUrl', 'description'],
-          include: [{model: Interest, attributes: ['id', 'name']}]
-        }
-      ]
-    })
-
-    res.status(200).send(savedContent)
   } catch (err) {
     console.error(err)
   }
--- server/api/users.js
+++ server/api/users.js
@@ -1,17 +1,26 @@
 const router = require('express').Router()
-const {User} = require('../db/models')
+const {User, Content, Interest} = require('../db/models')
 module.exports = router
 
-router.get('/', async (req, res, next) => {
+router.get('/:userId/content', async (req, res, next) => {
   try {
-    const users = await User.findAll({
-      // explicitly select only the id and email fields - even though
-      // users' passwords are encrypted, it won't help if we just
-      // send everything to anyone who asks!
-      attributes: ['id', 'firstName', 'lastName', 'username', 'email']
+    let userId = Number(req.params.userId)
+    let savedContent = await User.findAll({
+      where: {
+        id: userId
+      },
+      attributes: ['id'],
+      include: [
+        {
+          model: Content,
+          attributes: ['title', 'sourceUrl', 'imageUrl', 'description'],
+          include: [{model: Interest, attributes: ['id', 'name']}]
+        }
+      ]
     })
-    res.json(users)
+
+    res.status(200).send(savedContent)
   } catch (err) {
-    next(err)
+    console.error(err)
   }
 })
