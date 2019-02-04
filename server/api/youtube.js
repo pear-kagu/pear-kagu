71d59fd69b9e594d162f83e981062c462da277f3
VandyWebDev <39500384+VandyWebDev@users.noreply.github.com>
2019-02-04 17:32:30 -0500

    feat: youtube api with search param

--- server/api/youtube.js
+++ server/api/youtube.js
@@ -7,6 +7,8 @@ module.exports = router
 // set paramaters
 const TYPE_ID = 2
 const API_SOURCE_ID = 1
+const SEARCH_PARAMS = 'Technology'
+
 const youtubeApiKey = process.env.YOUTUBEAPI_KEY
 
 const youtube = google.youtube({
@@ -16,45 +18,45 @@ const youtube = google.youtube({
 
 router.get('/:interestId/:interestName', async (req, res, next) => {
   try {
-    let {interestId, interestName} = req.params
+    const {interestId, interestName} = req.params
+    const queryStr = `${interestName} ${SEARCH_PARAMS}`
     const searchResult = await youtube.search.list({
       part: 'id,snippet',
-      q: interestName,
+      q: queryStr,
       maxResults: 30,
       regionCode: 'US',
       relevanceLanguage: 'en',
-      type: 'video'
+      type: 'video',
+      order: 'date'
     })
 
     // deconstruct data
     const {items} = searchResult.data
-    await Promise.all(
-      items.map(async item => {
-        const title = item.snippet.title
-        const sourceUrl = `https://www.youtube.com/watch?v=${item.id.videoId}`
-        const imageUrl =
-          item.snippet.thumbnails.medium.url ||
-          'https://images.pexels.com/photos/97077/pexels-photo-97077.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500'
-        const description = item.snippet.description
-        const publishedAt = item.snippet.publishedAt
-        try {
-          await Content.findOrCreate({
-            where: {
-              title,
-              imageUrl,
-              description,
-              sourceUrl,
-              publishedAt,
-              typeId: TYPE_ID,
-              apiSourceId: API_SOURCE_ID,
-              interestId
-            }
-          })
-        } catch (err) {
-          console.log('Duplicated video not added')
-        }
-      })
-    )
+    items.map(async item => {
+      const title = item.snippet.title
+      const sourceUrl = `https://www.youtube.com/watch?v=${item.id.videoId}`
+      const imageUrl =
+        item.snippet.thumbnails.medium.url ||
+        'https://images.pexels.com/photos/97077/pexels-photo-97077.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500'
+      const description = item.snippet.description
+      const publishedAt = item.snippet.publishedAt
+      try {
+        await Content.findOrCreate({
+          where: {
+            title,
+            imageUrl,
+            description,
+            sourceUrl,
+            publishedAt,
+            typeId: TYPE_ID,
+            apiSourceId: API_SOURCE_ID,
+            interestId
+          }
+        })
+      } catch (err) {
+        console.log('Duplicated video not added')
+      }
+    })
     res.json(items)
   } catch (err) {
     console.log(err)
