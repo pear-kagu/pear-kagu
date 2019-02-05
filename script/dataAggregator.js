e1047740c707c07e6dc787b0734ec0a6832d8a7d
VandyWebDev <39500384+VandyWebDev@users.noreply.github.com>
2019-02-05 13:06:28 -0500

    feat: data aggregator for production

--- script/dataAggregator.js
+++ script/dataAggregator.js
@@ -2,7 +2,9 @@ const axios = require('axios')
 
 async function getInterests() {
   try {
-    const {data} = await axios.get('http://localhost:8080/api/interests')
+    const {data} = await axios.get(
+      'http://pear-kagu.herokuapp.com/api/interests'
+    )
     return data
   } catch (err) {
     console.error(err)
@@ -15,18 +17,18 @@ async function callApis() {
 
     for (let i = 0; i < allInterests.length; i++) {
       await axios.get(
-        `http://localhost:8080/api/newsapi/${allInterests[i].id}/${
+        `http://pear-kagu.herokuapp.com/api/newsapi/${allInterests[i].id}/${
           allInterests[i].name
         }`
       )
       await axios.get(
-        `http://localhost:8080/api/youtube/${allInterests[i].id}/${
+        `http://pear-kagu.herokuapp.com/api/youtube/${allInterests[i].id}/${
           allInterests[i].name
         }`
       )
       if (allInterests[i].name.split(' ').length === 1) {
         await axios.get(
-          `http://localhost:8080/api/meetups/${allInterests[i].id}/${
+          `http://pear-kagu.herokuapp.com/api/meetups/${allInterests[i].id}/${
             allInterests[i].name
           }`
         )
